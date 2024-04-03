f8c624e0b6d45a85ec53e9fbfedab661ffa9c499
HarisShaikh97 <harisahmedsheikh1997@gmail.com>
2024-04-04 02:35:17 +0500

    Snapshot capture and remove funtionality

--- background.js
+++ background.js
@@ -1,26 +1,49 @@
-chrome?.runtime?.onMessage?.addListener((message, sender, sendResponse) => {
-	if (message?.type === "capture") {
-		chrome?.tabs?.query({ active: true }, (tabs) => {
-			if (tabs && tabs.length > 0) {
-				const url = tabs[0]?.url
-				const tabId = tabs[0].id
-				console.log(url, tabId)
-				try {
-					chrome?.scripting?.executeScript({
-						target: { tabId: tabId },
-						files: ["./content.js"]
+chrome?.runtime?.onMessage?.addListener(
+	async (message, sender, sendResponse) => {
+		if (message?.type === "capture") {
+			await chrome?.tabs?.query({ active: true }, async (tabs) => {
+				if (tabs && tabs?.length > 0) {
+					const tabId = tabs[0]?.id
+					await chrome?.tabs?.sendMessage(tabId, {
+						type: "capture-snapshot"
 					})
-				} catch (error) {
-					console.error("Error capturing snapshot:", error)
+				} else {
+					console.log("No active tab found!")
 				}
-			} else {
-				console.log("No active tab found!")
-			}
-		})
+			})
+		}
+		if (message?.type === "captured-snapshot") {
+			const url = message?.url
+			const bodyContent = message?.data
+			await chrome?.storage?.local?.set({ [url]: bodyContent })
+		}
+		if (message?.type === "reset") {
+			await chrome?.tabs?.query({ active: true }, async (tabs) => {
+				if (tabs && tabs?.length > 0) {
+					const url = tabs[0]?.url
+					const tabId = tabs[0].id
+					await chrome?.storage?.local?.remove(url, async () => {
+						console.log("Snapshot removed for:", url)
+						await chrome?.tabs?.reload(tabId)
+					})
+				} else {
+					console.log("No active tab found!")
+				}
+			})
+		}
+		if (message?.type === "reset-all") {
+			await chrome?.storage?.local?.clear(async () => {
+				console.log("All snapshots removed")
+				await chrome?.tabs?.query({ active: true }, async (tabs) => {
+					if (tabs && tabs?.length > 0) {
+						const tabId = tabs[0].id
+						await chrome?.tabs?.reload(tabId)
+					} else {
+						console.log("No active tab found!")
+					}
+				})
+			})
+		}
+		console.log(message)
 	}
-	if (message?.type === "captured-snapshot") {
-		const deserializedHTML = JSON.parse(message?.data)
-		console.log(deserializedHTML)
-	}
-	console.log(message)
-})
+)
--- content.js
+++ content.js
@@ -1,9 +1,39 @@
-;(() => {
-	const capturedHTML = document.documentElement.outerHTML
-	const serializedHTML = JSON.stringify(capturedHTML)
-	console.log(serializedHTML)
-	chrome.runtime.sendMessage({
-		type: "captured-snapshot",
-		data: serializedHTML
-	})
+;(async () => {
+	const url = window?.location?.href
+	await chrome?.runtime?.onMessage?.addListener(
+		async (message, sender, sendResponse) => {
+			if (message?.type === "capture-snapshot") {
+				try {
+					const bodyContent = document?.body?.outerHTML
+					await chrome?.runtime?.sendMessage({
+						type: "captured-snapshot",
+						data: bodyContent,
+						url: url
+					})
+				} catch (error) {
+					console.error("Error capturing snapshot:", error)
+				}
+			}
+		}
+	)
+	if (
+		document?.readyState === "complete" ||
+		document?.readyState === "interactive"
+	) {
+		await chrome?.storage?.local?.get(url, (data) => {
+			const savedHTML = data[url]
+			if (savedHTML) {
+				document.body.innerHTML = savedHTML
+			}
+		})
+	} else {
+		document?.addEventListener("DOMContentLoaded", async () => {
+			await chrome?.storage?.local?.get(url, (data) => {
+				const savedHTML = data[url]
+				if (savedHTML) {
+					document.body.innerHTML = savedHTML
+				}
+			})
+		})
+	}
 })()
